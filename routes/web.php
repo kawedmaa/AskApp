01865b3a834a58e96ab3410d213044f6ac50b93a
kawedma <kawedma@gmail.com>
2021-03-24 07:17:50 +0300

    Display all Questions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\QuestionsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +22,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
+Route::any('/questions', [QuestionsController::class,'index']);
